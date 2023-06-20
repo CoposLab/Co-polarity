278dd4485e98125f794a0b6cd86b4870a1c1f298
levandosky-k <levandosky.k@northeastern.edu>
2023-06-20 18:13:17 -0400

    update pde solver

--- TwoCellCode/PDESolver1_1.m
+++ TwoCellCode/PDESolver1_1.m
@@ -1,3 +1,5 @@
+clf;
+
 Da      = 0.5;                  % diffusion coefficient for actin
 m0      = 2.0;                  % competition for actin monomers
 K       = 1.0;  
@@ -14,8 +16,8 @@ Xb      = 0:dxa:L;
 Da = 0.5; % diffusion coefficient
 pa = dt*Da/(dxa^2);
 
-Ka = 1; % branched coeff
-Kb = 1; % bundled coeff
+Ka = 2; % branched coeff on overlap
+Kb = 1.0; % bundled coeff on overlap
 alpha = 2;
 
 F = @(U,V) -m0*U.*V;
@@ -23,6 +25,11 @@ F = @(U,V) -m0*U.*V;
 a1 = 0.1 + 0.9.*rand(Na,1);
 b1 = 0.1 + 0.9.*rand(Na,1);
 
+bper=0.25;
+blen=floor(bper*Na);
+bound = (floor(Na/2)-floor(blen/2)):(floor(Na/2)+floor(blen/2));
+
+showplots = 0;
 vid = 0;
 vidObj = VideoWriter('PDESolver1','MPEG-4');
 
@@ -45,22 +52,72 @@ for i=1:Nt
     rxna1 = dt*(F(a1,b1) + K*(a1 - a1.*a1));
     rxnb1 = dt*(F(b1,a1) + K*(b1 - b1.*b1));
 
+    rxna1(bound) = dt*(F(a1(bound),b1(bound)) + Ka*(a1(bound) - a1(bound).*a1(bound)));
+    rxnb1(bound) = dt*(F(b1(bound),a1(bound)) + Kb*(b1(bound) - b1(bound).*b1(bound)));
+
 
     a1 = Hs1\(diffRHSa1+rxna1);
     b1 = Hs1\(diffRHSb1+rxnb1);
 
-    figure(1)
-    plot(Xa,a1,'-o','markerfacecolor',[159 219 229]/255,'linewidth',3); hold on;
-    plot(Xa,b1,'-ok','markerfacecolor','k','linewidth',3);
-    set(gca,'fontname','times','fontsize',20); box on;
-    set(gcf,'color','w');
-    legend('branched','bundled'); 
-    ylim([-1 1])
-    hold off
+    if showplots==1 || vid==1
+        figure(1)
+        plot(Xa,a1,'-o','markerfacecolor',[159 219 229]/255,'linewidth',3); hold on;
+        plot(Xa,b1,'-ok','markerfacecolor','k','linewidth',3);
+        set(gca,'fontname','times','fontsize',20); box on;
+        set(gcf,'color','w');
+        legend('branched','bundled');
+        ylim([-1 1])
+        hold off
 
-    if vid==1
-        currframe = getframe(gcf);
-        writeVideo(vidObj,currframe);
+        if vid==1
+            currframe = getframe(gcf);
+            writeVideo(vidObj,currframe);
+        end
     end
 
+end
+
+figure(1)
+plot(Xa,a1,'-o','markerfacecolor',[159 219 229]/255,'linewidth',3); hold on;
+plot(Xa,b1,'-ok','markerfacecolor','k','linewidth',3);
+set(gca,'fontname','times','fontsize',20); box on;
+set(gcf,'color','w');
+legend('branched','bundled');
+ylim([-1 1])
+hold off
+
+% Determine if cell is polarized
+a1New = a1;
+a1New(a1New<0.1)=0;
+if (a1New(1)~=0 && a1New(length(a1New))~=0)
+    zeroInd1=find(a1New==0,1,'first');
+    zeroInd2=find(a1New==0,1,'last');
+    dirIndex1=ceil((zeroInd1+zeroInd2)/2) - 50;
+else
+    ind1=find(a1New~=0,1,'first');
+    ind2=find(a1New~=0,1,'last');
+    dirIndex1=ceil((ind1+ind2)/2);
+end
+b1New = b1;
+b1New(b1New<0.1)=0;
+if (b1New(1)~=0 && b1New(length(b1New))~=0)
+    zeroInd1=find(b1New==0,1,'first');
+    zeroInd2=find(b1New==0,1,'last');
+    dirIndex2=ceil((zeroInd1+zeroInd2)/2) - 50;
+else
+    ind1=find(b1New~=0,1,'first');
+    ind2=find(b1New~=0,1,'last');
+    dirIndex2=ceil((ind1+ind2)/2);
+end
+if dirIndex1<1
+    dirIndex1=dirIndex1+101;
+end
+if dirIndex2<1
+    dirIndex2=dirIndex2+101;
+end
+
+if isempty(dirIndex1) || isempty(dirIndex2)
+    sprintf('Not Polarized')
+else
+    sprintf('Polarized')
 end
